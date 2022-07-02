3f845acd5f4132ba3f6fc6408d710322320d494d
Sammy5k <46389011+Sammy5k@users.noreply.github.com>
2022-07-02 16:26:45 -0700

    PlayerMovement Iteration 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,37 +3,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed; //Unity Speed Parameter
-    private Rigidbody2d body; // integrate asset
+    private Rigidbody2D body; // integrate asset
     private Animator anim;
     private bool grounded;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2d>();
+        body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
     //move left right
     private void Update() // User input
 	{
-		float horizontalInput = Input.GetAxis("Horizontal"); # stores value
+		float horizontalInput = Input.GetAxis("Horizontal");
 		body.velocity = new Vector2(horizontalInput * speed,body.velocity.y);
 		
 		if(horizontalInput > 0.01f)
 			transform.localScale = Vector3.one;
-		else if (horizonatalInput < -0.01f)
+		else if (horizontalInput < -0.01f)
 			transform.localScale = new Vector3(-1,1,1);
 		
-		if(Input.GetKey(KeyCode.Space)) && grounded) //jump when grounded
+		if(Input.GetKey(KeyCode.Space) && grounded) //jump when grounded
 			body.velocity = new Vector2(body.velocity.x, speed); 
 			
 		//set animator parameters
 		anim.SetBool("run", horizontalInput != 0); //run into idle; idle into run
-		anim.SetBool("grounded", grounded) //run into jump; jump end
+		anim.SetBool("grounded", grounded); //run into jump; jump end
 	}
 	//jump
 	private void Jump()
 	{
 		body.velocity = new Vector2(body.velocity.x, speed);
+		anim.SetTrigger("jump");
 		grounded = false;
 	}
 	
