b8b0d22f1367f9c97c4c095a0f0bfdc884313b6f
Sammy5k <46389011+Sammy5k@users.noreply.github.com>
2022-07-02 22:06:38 -0700

    Commented out Anim; Variable horizontalInput Fixed (#7)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed; //Unity Speed Parameter
     private Rigidbody2D body; // integrate asset
-    private Animator anim;
+    //private Animator anim; TODO Implement Animator
     private bool grounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
     }
     //move left right
     private void Update() // User input
@@ -27,28 +27,27 @@ public class PlayerMovement : MonoBehaviour
 		if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && grounded) //jump when grounded
 			Jump();
 		//set animator parameters
-		anim.SetBool("run", horizontalInput != 0); //run into idle; idle into run
-		anim.SetBool("grounded", grounded); //run into jump; jump end
-		float horizontalInput = Input.GetAxis("Horizontal");
+		//anim.SetBool("run", horizontalInput != 0); //run into idle; idle into run
+		//anim.SetBool("grounded", grounded); //run into jump; jump end
 		body.velocity = new Vector2(horizontalInput * speed,body.velocity.y);
 		
 		if (horizontalInput > 0.01f)
 			transform.localScale = Vector3.one;
-		else if (horizonatalInput < -0.01f)
+		else if (horizontalInput < -0.01f)
 			transform.localScale = new Vector3(-1,1,1);
 		
-		if (Input.GetKey(KeyCode.Space)) && grounded) //jump when grounded
+		if (Input.GetKey(KeyCode.Space) && grounded) //jump when grounded
 			body.velocity = new Vector2(body.velocity.x, speed); 
 			
 		//set animator parameters
-		anim.SetBool("run", horizontalInput != 0); //run into idle; idle into run
-		anim.SetBool("grounded", grounded) //run into jump; jump end
+		//anim.SetBool("run", horizontalInput != 0); //run into idle; idle into run
+		//anim.SetBool("grounded", grounded); //run into jump; jump end
 	}
 	//jump
 	private void Jump()
 	{
 		body.velocity = new Vector2(body.velocity.x, speed);
-		anim.SetTrigger("jump");
+		//anim.SetTrigger("jump");
 		grounded = false;
 	}
 	
