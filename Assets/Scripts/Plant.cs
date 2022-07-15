50c5fd2c3e769059babfd51b9e507174cacb5fbd
Collin Eng <eng.engineereng@gmail.com>
2022-07-15 12:26:12 -0700

    refactor plant and its children

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Plant : MonoBehaviour
 {
     public float life = 0.0f;
+    public bool isAlive = false;
     private readonly float lifeDecay = 0.05f;
     private readonly float LIFECAP = 100.0f;
     public void Heal (int healing)
@@ -16,21 +17,25 @@ public class Plant : MonoBehaviour
         }        
     }
 
-    private void ChangeColor()
+    protected void ChangeColor()
     {
         GetComponent<SpriteRenderer>().color = new Color (0.0f, life / LIFECAP, 0, 1.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DecayLife()
     {
-        
         life -= lifeDecay;
         if (life < 0)
         {
             life = 0;
         }
-        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecayLife();
+        isAlive = life > 50;
         ChangeColor();
     }
 }
